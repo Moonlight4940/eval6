eade6262d103dd6b442722871d21360e0b6960f7
Moonlight4940 <alex4940@hotmail.com>
2025-03-02 12:51:06 +0100

    modification

--- dump.sql
+++ dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 21, 2025 at 10:20 PM
+-- Generation Time: Mar 02, 2025 at 11:45 AM
 -- Server version: 10.3.39-MariaDB-0ubuntu0.20.04.2
 -- PHP Version: 8.3.16
 
@@ -43,16 +43,16 @@ CREATE TABLE `etudiants` (
 --
 
 INSERT INTO `etudiants` (`id`, `prenom`, `nom`, `email`, `cv`, `dt_naissance`, `isAdmin`, `dt_mise_a_jour`) VALUES
-(1, 'Alain', 'Dupont', 'alain@free.fr', 'CV de l\'étudiant', '1995-03-10', 0, '2025-02-21 21:47:10'),
 (2, 'Bruno', 'Mars', 'bruno@hotmail.com', 'CV de l\'étudiant', '1980-09-30', 0, '2025-02-21 21:49:53'),
 (3, 'Lara', 'Fabien', 'lara@gmail.com', 'CV de l\'étudiant', '1983-12-25', 0, '2025-02-21 21:52:31'),
-(4, 'Justin', 'Beaver', 'justin@hotmail.com', 'CV de l\'étudiant', '1995-04-29', 0, '2025-02-21 21:54:12'),
 (5, 'Celine', 'Dion', 'celine@yahoo.com', 'CV de l\'étudiant', '1980-08-07', 0, '2025-02-21 21:57:11'),
 (6, 'Dua', 'Lipa', 'dualipa@hotmail.fr', 'CV de l\'étudiant', '2002-05-09', 0, '2025-02-21 21:58:38'),
 (7, 'Billie', 'Eilish', 'billie@gmail.com', 'CV de l\'étudiant', '2004-05-08', 0, '2025-02-21 22:00:13'),
 (8, 'James', 'Brown', 'james@gmail.com', 'CV de l\'étudiant', '1968-05-19', 0, '2025-02-21 22:01:37'),
 (9, 'Amy', 'Winehouse', 'awinehouse@hotmail.com', 'CV de l\'étudiant', '1992-09-24', 0, '2025-02-21 22:03:34'),
-(10, 'Julien', 'Doré', 'julien@gmail.com', 'CV de l\'étudiant', '1989-11-19', 0, '2025-02-21 22:12:31');
+(10, 'Julien', 'Doré', 'julien@gmail.com', 'CV de l\'étudiant', '1989-11-19', 0, '2025-02-21 22:12:31'),
+(11, 'Tom', 'Cruz', 'tom@gmail.com', 'monCV', '1979-05-14', 0, '2025-03-01 10:21:14'),
+(12, 'Louane', 'Emera', 'louane@gmail.com', 'monCV', '2000-05-19', 0, '2025-03-02 08:20:50');
 
 --
 -- Indexes for dumped tables
@@ -72,7 +72,7 @@ ALTER TABLE `etudiants`
 -- AUTO_INCREMENT for table `etudiants`
 --
 ALTER TABLE `etudiants`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
